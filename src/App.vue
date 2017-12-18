2d292c5b52bfc8561dddf5a474f7a6ed056eb6f5
Cicko <rudolfcicko2016@gmail.com>
2017-12-18 23:01:54 +0000

    showing the form in the app

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,18 @@
     <v-toolbar fixed app>
     </v-toolbar>
     <v-content>
+      <my-form class="mx-5">
+      </my-form>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import myForm from './Form.vue'
   export default {
+    components: {
+      myForm,
+    },
     data () {
       return {
       }
