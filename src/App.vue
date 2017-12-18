cbacfc424e30b044970c6a321f038acb40aa9f5d
Cicko <rudolfcicko2016@gmail.com>
2017-12-18 23:06:27 +0000

    Creating a grid container for both components

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,18 @@
     <v-toolbar fixed app>
     </v-toolbar>
     <v-content>
-      <my-form class="mx-5">
-      </my-form>
-      <my-list class="mx-5">
-      </my-list>
+      <v-container grid-list-md text-xs-center>
+        <v-layout row wrap>
+          <v-flex class="xs6">
+            <my-form class="mx-5">
+            </my-form>
+          </v-flex>
+          <v-flex class="xs6">
+            <my-list class="mx-5">
+            </my-list>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
   </v-app>
 </template>
