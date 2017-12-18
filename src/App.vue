044eb149143a6e2d6bc41fa4515373113f2c43fb
Cicko <rudolfcicko2016@gmail.com>
2017-12-18 23:12:31 +0000

    Added switch to change view with automatic component resize

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,21 @@
     <v-content>
       <v-container grid-list-md text-xs-center>
         <v-layout row wrap>
-          <v-flex class="xs6">
+          <v-flex v-if="view_inx === 0 || view_inx === 1"
+            :class="view_inx === 0 ? 'xs6' : 'xs12'">
             <my-form class="mx-5">
             </my-form>
           </v-flex>
-          <v-flex class="xs6">
+          <v-flex v-if="view_inx === 0 || view_inx === 2"
+            :class="view_inx === 0 ? 'xs6' : 'xs12'">
             <my-list class="mx-5">
             </my-list>
           </v-flex>
         </v-layout>
       </v-container>
+      <div class="text-xs-center">
+        <v-btn primary @click="switchView">{{actual_view}}</v-btn>
+      </div>
     </v-content>
   </v-app>
 </template>
@@ -23,6 +28,12 @@
 import myForm from './Form.vue'
 import myList from './List.vue'
 
+const Views = [
+  'Form + List',
+  'Only Form',
+  'Only List'
+]
+
 export default {
   components: {
     myForm,
@@ -30,7 +41,16 @@ export default {
   },
   data () {
     return {
+      views: Views,
+      view_inx: 0,
+      actual_view: Views[0],
     }
+  },
+  methods: {
+    switchView () {
+      this.view_inx = ++this.view_inx % Views.length
+      this.actual_view = Views[this.view_inx]
+    },
   }
 }
 </script>
